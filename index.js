50a6fbdd70c4b5a6f50b18d91340e8073427c1ac
i-niranjan <niranjanchaudhari2004@gmail.com>
2026-01-29 05:39:36 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+import express from "express";
+
+const app = express();
+
+const PORT = process.env.PORT ?? 8080;
+
+app.get("/", (req, res) => {
+  return res.json({ msg: "Hello from the server" });
+});
+
+const server = app.listen(PORT, () => {
+  console.log(`Server is up and running on PORT => ${PORT}`);
+});
+
+const shutdown = () => {
+  console.log("Shutting down...");
+  server.close(() => {
+    process.exit(0);
+  });
+};
+
+process.on("SIGINT", shutdown);
+process.on("SIGTERM", shutdown);
